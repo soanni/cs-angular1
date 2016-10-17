29e11619395b69e2c48e6e3703712e1440d29a7b
Andrey Solodov <legandr.86@gmail.com>
2016-10-17 21:35:56 +0400

    first component

---
+++ app/maint.ts
@@ -0,0 +1,25 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'racing-app',
+  template: `
+  <h1>{{heading}}</h1>
+  <h2>{{race.name}}</h2>
+  <p>{{race.date}}</p>
+  <p>{{race.about}}</p>
+  `
+})
+
+class AppComponent {
+  heading = "Ultra Racing Schedule"
+  race = {
+    "id": 1,
+    "name": "Daytona Thunderdome",
+    "date": new Date('2512-01-04T14:00:00'),
+    "about": "Race through the ruins of an ancient Florida battle arena.",
+    "entryFee": 3200
+  }
+}
+
+bootstrap(AppComponent);
